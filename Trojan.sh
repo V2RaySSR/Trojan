7e3993e41c2df761e5e1f8f0b24efeb7f50a1d56
V2RaySSR <59167743+V2RaySSR@users.noreply.github.com>
2020-01-07 16:33:26 +0800

    Update Trojan.sh

--- Trojan.sh
+++ Trojan.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 #fonts color
-yellow(){echo -e "\033[33m"}
-green(){echo -e "\033[32m"}
-red(){echo -e "\033[31m"}
+Yellow(){
+    echo -e "\033[33m\033[01m$1\033[0m"
+}
+green(){
+    echo -e "\033[32m\033[01m$1\033[0m"
+}
+red(){
+    echo -e "\033[31m\033[01m$1\033[0m"
+}
 
 if [[ -f /etc/redhat-release ]]; then
     release="centos"
